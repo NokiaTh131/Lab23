678c0f304c9ef5991f692f8750fc8d446d4f1810
= <=>
2023-03-06 23:59:37 +0700

    finish

--- calculator.cpp
+++ calculator.cpp
@@ -1,10 +1,100 @@
 #include <windows.h>
+#include <stdio.h>
+#include<string>
 
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
+	static char textSaved[20];
+	static char textSaved_2[20];
+	
+
+	static HWND label1, Textbox1, Textbox2;
+	
 	switch(Message) {
-		
+		case WM_CREATE:
+
+		label1 = CreateWindow("STATIC","Please input two numbers",WS_VISIBLE|WS_CHILD|WS_BORDER,20,20,200,20,hwnd,NULL,NULL,NULL);
+
+		Textbox1 = CreateWindow("EDIT","",WS_BORDER|WS_CHILD|WS_VISIBLE,45,45,150,20,hwnd,NULL,NULL,NULL);
+		Textbox2 = CreateWindow("EDIT","",WS_BORDER|WS_CHILD|WS_VISIBLE,45,70,150,20,hwnd,NULL,NULL,NULL);
+
+		CreateWindow("BUTTON","+",WS_VISIBLE|WS_CHILD|WS_BORDER,50,110,30,30,hwnd,(HMENU) 1,NULL,NULL);
+
+		CreateWindow("BUTTON","-",WS_VISIBLE|WS_CHILD|WS_BORDER,80,110,30,30,hwnd,(HMENU) 2,NULL,NULL);
+
+		CreateWindow("BUTTON","*",WS_VISIBLE|WS_CHILD|WS_BORDER,110,110,30,30,hwnd,(HMENU) 3,NULL,NULL);
+
+		CreateWindow("BUTTON","/",WS_VISIBLE|WS_CHILD|WS_BORDER,140,110,30,30,hwnd,(HMENU) 4,NULL,NULL);
+
+
+
+		break;
 		/* Upon destruction, tell the main thread to stop */
+		case WM_COMMAND:
+		switch (LOWORD(wParam)) 
+		{
+		case 1:
+		{
+			GetWindowText(Textbox1, &textSaved[0],21);
+			GetWindowText(Textbox2, &textSaved_2[0],21);
+			float x,y;
+			sscanf(textSaved, "%f", &x);
+			sscanf(textSaved_2, "%f", &y);
+			float sum = x + y;
+			char shoW[100];
+			sprintf(shoW, "%f", sum);
+			::MessageBox(hwnd, shoW, "Result", MB_OK);
+			break;
+		}
+		case 2:
+		{
+			GetWindowText(Textbox1, &textSaved[0],21);
+			GetWindowText(Textbox2, &textSaved_2[0],21);
+			float x,y;
+			sscanf(textSaved, "%f", &x);
+			sscanf(textSaved_2, "%f", &y);
+			float minus = x - y;
+			char shoW[100];
+			sprintf(shoW, "%f", minus);
+			::MessageBox(hwnd, shoW, "Result", MB_OK);
+			break;
+		}
+		case 3:
+		{
+			GetWindowText(Textbox1, &textSaved[0],21);
+			GetWindowText(Textbox2, &textSaved_2[0],21);
+			float x,y;
+			sscanf(textSaved, "%f", &x);
+			sscanf(textSaved_2, "%f", &y);
+			float times = x * y;
+			char shoW[100];
+			sprintf(shoW, "%f", times);
+			::MessageBox(hwnd, shoW, "Result", MB_OK);
+			break;
+		}
+		case 4:
+		{
+			GetWindowText(Textbox1, &textSaved[0],21);
+			GetWindowText(Textbox2, &textSaved_2[0],21);
+			float x,y;
+			sscanf(textSaved, "%f", &x);
+			sscanf(textSaved_2, "%f", &y);
+			float divide = x / y;
+			char shoW[100];
+			sprintf(shoW, "%f", divide);
+			::MessageBox(hwnd, shoW, "Result", MB_OK);
+			break;
+		}
+		
+
+		
+			
+				
+		}
+		break;
+
+
+
 		case WM_DESTROY: {
 			PostQuitMessage(0);
 			break;
@@ -31,7 +121,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	HBRUSH brush = CreateSolidBrush(RGB(200, 0, 0));
+	wc.hbrBackground = brush;
 	wc.lpszClassName = "WindowClass";
 	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
 	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
@@ -41,18 +132,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_VISIBLE|WS_SYSMENU,
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
+		250, /* width */
+		200, /* height */
 		NULL,NULL,hInstance,NULL);
 
 	if(hwnd == NULL) {
 		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
 		return 0;
 	}
-
+    SetClassLongPtr(hwnd, GCLP_HBRBACKGROUND, (LONG_PTR)brush);
 	/*
 		This is the heart of our program where all input is processed and 
 		sent to WndProc. Note that GetMessage blocks code flow until it receives something, so
